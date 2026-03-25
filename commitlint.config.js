b75cb332a94a30d0a9f58dd429b9647678b7115e
zntb <zenetibi@yahoo.com>
2026-03-25 12:17:48 +0200

    chore: add husky git hooks, commitlint, and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "perf",
+        "ci",
+        "build",
+      ],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+  },
+};
