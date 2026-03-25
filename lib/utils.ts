e456bb33d38c9013fe51d0fce9b86c6ed5ace860
zntb <zenetibi@yahoo.com>
2026-03-25 13:07:54 +0200

    test: verify block on unused variable error

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,4 +4,5 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-("const unusedTest = 'error';");
+
+const unusedTest = 'error';
